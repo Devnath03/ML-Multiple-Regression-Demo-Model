939cff612e50b05032e53eff7d73e2f1ee08d05a
Dev <jmdevnath.c@gmail.com>
2025-08-02 15:21:24 +0530

    Add Application

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import streamlit as st
+
+# Main application file for Streamlit
+st.title("Streamlit Test Application")
+
+# Main application description
+st.subheader("This is a simple test application to demonstrate Streamlit functionality.")
