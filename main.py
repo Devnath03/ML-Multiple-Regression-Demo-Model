1cedd8f0587f2a6be15e5f557b7147a7ed62e1fc
Dev <jmdevnath.c@gmail.com>
2025-08-03 20:46:48 +0530

    add python filr

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open("model.pickel","rb"))
 
 # Main application file for Streamlit
-st.title("Streamlit Test Application")
+st.title("Startup Profit Prediction")
 
-# Main application description
-st.subheader("This is a simple test application to demonstrate Streamlit functionality.")
+
+RD = st.text_input("R&D Spend", key="1")
+Administration = st.text_input("Administration", key="2")
+Marketing = st.text_input("Marketing", key="3")
+
+state = st.selectbox("Do You State:", ["California","Florida"])
+state_value = 1 if state == "California" else 0
+
+if st.button("Predict"):
+    inp = np.array([[float(RD), float(Administration), float(Marketing), float(state_value)]])
+    prediction = model.predict(inp)
+    st.success("profit:")
+ 
